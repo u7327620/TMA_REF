bd4a3f4d642eb1545ca01e60a726731efdfd2a97
u7327620 <u7327620@anu.edu.au>
2025-03-31 08:53:43 +1100

    simplified index.js

--- index.js
+++ index.js
@@ -1,85 +1,66 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const fs = require('fs');
+const { Client, Intents, Collection } = require('discord.js');
 
-const intents = new Array();
-
-intents.push(GatewayIntentBits.Guilds);
-intents.push(GatewayIntentBits.GuildMembers);
-intents.push(GatewayIntentBits.GuildMessages);
-intents.push(GatewayIntentBits.MessageContent);
-
-const client = new Client({ intents: intents });
-
-client.clips = new Collection();
-
-client.mod_message = new Collection();
-
-client.cross_roles = new Collection();
-
-fs.readFile('./cross-roles.json', 'utf8', (e, data) => {
-	if (e) {
-		console.error(e);
-		return;
-	}
-
-	const { guilds } = JSON.parse(data);
-
-	for (const {guild_id, cross_channel_id, cross_roles} of guilds) {
-		const roles = new Collection();
-		for (const {id, name} of cross_roles) {
-			roles.set(name, id);
-		}
-		client.cross_roles.set(guild_id, { cross_channel_id: cross_channel_id, cross_roles: roles });
-	}
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.MESSAGE_CONTENT]
 });
 
+const config = require('./config.json');
+client.config = config;
+client.clips = new Collection();
+client.mod_message = new Collection();
+client.cross_roles = new Collection();
+client.commands = new Collection();
 client.clips_setup = new Collection();
 
-fs.readFile('./clips-setup.json', 'utf8', (e, data) => {
-	if (e) {
-		console.error(e);
-		return;
-	}
+// asked majed what cross-roles is ...
+fs.readFile('./cross-roles.json', 'utf8', (e, data) => {
+  if (e) {
+    console.error(e);
+    return;
+  }
 
-	const { guilds } = JSON.parse(data);
+  const guilds = JSON.parse(data);
 
-	for (const {guild_id, setup} of guilds)
-		client.clips_setup.set(guild_id, setup);
+  for (const { guild_id, cross_channel_id, cross_roles } of guilds) {
+    const roles = new Collection();
+    for (const { id, name } of cross_roles) {
+      roles.set(name, id);
+    }
+    client.cross_roles.set(guild_id, { cross_channel_id: cross_channel_id, cross_roles: roles });
+  }
 });
 
-client.commands = new Collection();
+// also going to need to know what's in this file ...
+fs.readFile('./clips-setup.json', 'utf8', (e, data) => {
+  if (e) {
+    console.error(e);
+    return;
+  }
 
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+  const guilds = JSON.parse(data);
 
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
+  for (const { guild_id, setup } of guilds)
+    client.clips_setup.set(guild_id, setup);
+});
+
+
+// binds events from filename
+const events = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
+for (const file of events) {
+  const eventName = file.split(".")[0];
+  const event = require(`./events/${file}`);
+  client.on(eventName, event.bind(null, client));
 }
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+// adds commands to a list from filename
+const commands = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+for (const file of commands) {
+  const commandName = file.split(".")[0];
+  const command = require(`./commands/${file}`);
 
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(client, ...args));
-	}
+  console.log(`Attempting to load command ${commandName}`);
+  client.commands.set(commandName, command);
 }
 
 client.login(token);
